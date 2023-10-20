b2dd3d2afb312836bb41ec7ca771cc615fc4850b
Kolin Guo <ruguo@ucsd.edu>
2023-10-20 16:24:44 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name='active_zero2',
+        version='0.0.1',
+        description="ActiveZero++",
+        install_requires=[],
+        packages=find_packages(include=["active_zero2*"]),
+    )
